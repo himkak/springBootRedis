5f62f239bc5054a789b82048bcae3c01286aac75
himanshu <hmnshkakar300@gmail.com>
2019-06-04 23:39:23 +0530

    removed enabel cache annotation

--- src/main/java/com/spring/cache/redis/SpringBootRedisApplication.java
+++ src/main/java/com/spring/cache/redis/SpringBootRedisApplication.java
@@ -2,10 +2,9 @@ package com.spring.cache.redis;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cache.annotation.EnableCaching;
 
 @SpringBootApplication
-@EnableCaching
+//@EnableCaching
 public class SpringBootRedisApplication {
 
 	public static void main(String[] args) {
